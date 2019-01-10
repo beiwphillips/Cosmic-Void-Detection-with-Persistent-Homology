690d8c669c46bf1ca23270b7dd4c86e204eb3593
Vikrant Kamble <vikrant.kamble1990@gmail.com>
2019-01-09 22:30:57 -0700

    changed interpolate

--- interpolation/Interpolate.py
+++ interpolation/Interpolate.py
@@ -1,89 +1,98 @@
 #!/usr/bin/env python
-
-
 import sys
-import math
 import subprocess
-import timeit
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+from data_wrangle import celestial_rot_matrix
 from astropy.io import fits
 
-from plotly.offline import plot
+from plotly.offline import plot as go_plot
 import plotly.graph_objs as go
 
 
 class Interpolate:
-    def __init__(self, filepath=None, sample_size=None, sample_rate=None, noise=0):
+    def __init__(self, filepath=None):
         if filepath is None:
             filepath = sys.argv[1]
-        self.data = fits.open(filepath)
-        self.Q = None
-        self.p = None
+        self.data_file = fits.open(filepath)
 
-        self.get_matrix()
-        self.get_data(sample_size, sample_rate, noise)
+        self.rot_matrix = None
+        self.pixel_data = None
 
-    def get_matrix(self):
-        print('******CALCULATING ROTATION MATRIX********')
-        # extract the ra, dec and quasar redshift for each object
-        # append a dummy index at the first location since the table data in the fits
-        # file starts at index 1
-        ra_list, dec_list, z_list = [np.nan], [np.nan], [np.nan]
+        self.ra = None
+        self.dec = None
+        self.zq = None
 
-        for i in range(1, len(self.data)):
-            header = self.data[i].header
-            ra_list.append(header['RA'])
-            dec_list.append(header['DEC'])
-            # z_list.append(header['Z'])
+        self.spacing = 4
 
-        ra, dec = np.deg2rad(np.array(ra_list)), np.deg2rad(np.array(dec_list))
-        mean_ra, mean_dec = np.nanmean(ra), np.nanmean(dec)
+        # self.get_matrix()
+        # self.get_data(sample_size, sample_rate, noise)
 
-        # The k-vector pointing along the direction of LOS
-        k_par = np.array([np.sin(mean_dec) * np.cos(mean_ra), np.sin(mean_dec) * np.sin(mean_ra), np.cos(mean_dec)])
+    def __repr__(self):
+        repr_str = "***** Interpolate Object ***** \n" + \
+            "Number of skewers are {}".format(len(self.data_file) - 1)
+        return repr_str
 
-        # Vector perpendicular to the above vector over which we rotate the co-ordinate system
-        k_perp = np.array([-np.sin(mean_ra), np.cos(mean_ra), 0])
+    def __len__(self):
+        return len(self.data_file) - 1
 
-        # Rotation direction - Cross product matrix
-        K = np.array([
-            [0, -k_perp[2], k_perp[1]],
-            [k_perp[2], 0, -k_perp[0]],
-            [-k_perp[1], k_perp[0], 0]
-        ])
+    def get_locations(self):
+        """ Get the angular positions and the redshift
+        of the skewers 
+        """
+        ra_list, dec_list, zq_list = [], [], []
+        for i in range(1, len(self.data_file)):
+            hdr = self.data_file[i].header
 
-        # ADOPTED FROM: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-        # Rotation Matrix to rotate the basis vectors
-        R = np.eye(3) + np.sin(mean_dec) * K + (1 - np.cos(mean_dec)) * np.matmul(K, K)
+            ra_list.append(hdr['RA'])
+            dec_list.append(hdr['DEC'])
+            zq_list.append(hdr['Z'])
 
-        # Rotation Matrix to rotate the points vectors
-        Q = np.eye(3) - np.sin(mean_dec) * K + (1 - np.cos(mean_dec)) * np.matmul(K, K)
+        self.ra = np.deg2rad(ra_list)
+        self.dec = np.deg2rad(dec_list)
+        self.zq = zq_list
 
-        print('Rotation Matrix: ')
-        print(Q)
+    def get_matrix(self, center=None):
+        """ Rotate the celestial sphere such that the polar axis
+        points towards the central ra and dec of the data patch
+        """
+        print("****** CALCULATING ROTATION MATRIX ********")
+        if center is None:
+            center = [np.nanmean(self.ra), np.nanmean(self.dec)]
 
-        self.Q = Q
+        self.rot_matrix = celestial_rot_matrix(*center, is_rad=True)
 
-    def get_data(self, sample_size, sample_rate, noise, viz=False):
-        print('******READING******')
-        length = len(self.data)
-        if sample_size is not None:
-            idx = np.random.choice(np.arange(1, length), size=sample_size, replace=False)
-        elif sample_rate is not None:
-            idx = np.random.choice(np.arange(1, length), size=int((length-1)*sample_rate), replace=False)
-        else:
-            raise ValueError()
+    def get_data(self, skewers_num=None, skewers_perc=1., noise=0.01):
+        """ Load skewer data and convert to cartesian cordinates
+
+        Parameters:
+        ===========
+        skewers_num : select a given number of skewers
+        skewers_perc : select a given percentage of skewers
+        noise : measurement noise
+        """
+        if skewers_num is None:
+            skewers_num = int((len(self) - 1) * skewers_perc)
+        ixs = np.random.choice(np.arange(1, len(self)), size=skewers_num,
+                               replace=False)
 
         x_list, y_list, z_list, v_list = [], [], [], []
-        for i in idx:
-            x, y, z, v = self.read_line(self.data[i])
+
+        print('****** READING ******')
+        for i in ixs:
+            rcomov = self.data_file[i].data['RCOMOV']
+            value = self.data_file[i].data['DELTA_T']
+
+            x = np.sin(self.dec[i-1]) * np.cos(self.ra[i-1]) * rcomov
+            y = np.sin(self.dec[i-1]) * np.sin(self.ra[i-1]) * rcomov
+            z = np.cos(self.dec[i-1]) * rcomov
+
             x_list.append(x)
             y_list.append(y)
             z_list.append(z)
-            v_list.append(v)
+            v_list.append(value)
 
         # concatenating together
         x = np.hstack(x_list)
@@ -92,99 +101,87 @@ class Interpolate:
         v = np.hstack(v_list)
 
         # rotating the points
-        x, y, z = np.dot(self.Q, np.array([x, y, z]))
-
-        # shifting cordinates
-        x -= x.min() - 5
-        y -= y.min() - 5
-        z -= z.min() - 5
+        x, y, z = np.dot(self.rot_matrix, np.array([x, y, z]))
 
         # generating noise data
         n = np.ones_like(x) * noise
 
         # concatenating together
-        self.p = np.vstack([x, y, z, n, v]).T
+        self.pixel_data = np.vstack([x, y, z, n, v]).T
 
-        if viz:
-            self.visualize3d(x, y, z)
+        print('Read {} points from the file'.format(len(x)))
 
-        print('Read {} points from the file'.format(self.p.shape[0]))
+    @staticmethod
+    def visualize3d(pixel_data, plot_1d, plot_2d):
+        if plot_1d:
+            fig, ax = plt.subplots(ncols=3)
+            for i in range(3):
+                sns.distplot(pixel_data[:, i], ax=ax[i], kde=False)
+        plt.show()
 
-    def read_line(self, line):
-        ra = (line.header['RA']) * np.pi / 180
-        dec = (line.header['DEC']) * np.pi / 180
-        rcomov = line.data['RCOMOV']
-        value = line.data['DELTA_T']
+        if plot_2d:
+            plt.figure()
+            plt.scatter(pixel_data[:, 0], pixel_data[:, 1], s=0.1, c="k")
+        plt.show()
 
-        x = np.sin(dec) * np.cos(ra) * rcomov
-        y = np.sin(dec) * np.sin(ra) * rcomov
-        z = np.cos(dec) * rcomov
-        v = value
+        trace = go.Scatter3d(x=pixel_data[:, 0], y=pixel_data[:, 1],
+                             z=pixel_data[:, 2], mode='markers',
+                             marker=dict(size=1, opacity=0.5))
+        fig = go.Figure(data=[trace])
+        go_plot(fig)
 
-        return x, y, z, v
+    def process_box(self, xcuts=None, ycuts=None, zcuts=None):
+        xx, yy, zz = self.pixel_data[:, :3].T
 
-    def process(self, chunk_size=10000):
-        print('******PROCESSING******')
-        z = self.p[:,2]
-        zmin = math.floor(z.min())
-        zmax = math.ceil(z.max())
-        start = zmin
-        end = zmin
-        cnt = 0
-        while end <= zmax:
-            while end <= zmax:
-                idx = ((z >= start) & (z < end))
-                if idx.sum() >= chunk_size:
-                    break
-                end += 1
-            print('Processing z range: {} - {}'.format(start, end))
-            self.dachshund(self.p[idx], cnt)
-            start = end
-            cnt += 1
-        print('******FINISHED******')
+        if xcuts is None:
+            xcuts = [xx.min(), xx.max()]
+        if ycuts is None:
+            ycuts = [yy.min(), yy.max()]
+        if zcuts is None:
+            zcuts = [zz.min(), zz.max()]
 
-    def process_one(self, chunk_size=10000, viz=False):
-        print('******PROCESSING******')
-        z = self.p[:,2]
-        zmin = math.floor(z.min())
-        zmax = math.ceil(z.max())
-        start = zmin
-        end = zmin
-        while end <= zmax:
-            idx = ((z >= start) & (z < end))
-            if idx.sum() >= chunk_size:
-                break
-            end += 1
-        print('Processing z range: {} - {}'.format(start, end))
-        self.dachshund(self.p[idx], 0)
-        if viz:
-            self.visualize_interpolation(0, self.p[idx, 0], self.p[idx, 1], self.p[idx, 2])
-            self.visualize3d(self.p[idx, 0], self.p[idx, 1], self.p[idx, 2])
-        print('******FINISHED******')
+        ixs = (xx >= xcuts[0]) & (xx < xcuts[1]) &\
+              (yy >= ycuts[0]) & (yy < ycuts[1]) &\
+              (zz >= zcuts[0]) & (zz < zcuts[1])
 
-    def dachshund(self, chunk, i):
+        # shift the cordinates to the edge of the cube
+        self.chunk = self.pixel_data[ixs]
 
+        self.chunk[:, 0] -= self.chunk[:, 0].min() + 5
+        self.chunk[:, 1] -= self.chunk[:, 1].min() + 5
+        self.chunk[:, 2] -= self.chunk[:, 2].min() + 5
+
+        # self.visualize3d(self.chunk, True, True)
+
+        self.dachshund()
+
+    def dachshund(self, offset=5, counter=0):
         # write binary file
-        chunk.tofile('pixel_data.bin')
+        self.chunk.tofile('pixel_data.bin')
 
         # calculate values
-        x = chunk[:,0]
-        y = chunk[:,1]
-        z = chunk[:,2]
+        xx, yy, zz = self.chunk[:, :3].T
 
-        npx_x = int(np.floor((x.max() - x.min()) / 2.))
-        npx_y = int(np.floor((y.max() - y.min()) / 2.))
-        npx_z = int(np.floor((z.max() - z.min()) / 2.))
+        # number of points in the grid
+        self.lx = xx.max() - offset
+        self.ly = yy.max() - offset
+        self.lz = zz.max() - offset
+
+        self.npx_x = int((xx.max() - offset) // self.spacing)
+        self.npx_y = int((yy.max() - offset) // self.spacing)
+        self.npx_z = int((zz.max() - offset) // self.spacing)
+        print("dimensions of the grid are {}{}{}".format(self.npx_x,
+                                                         self.npx_y, self.npx_z))
 
         # write config file
         cf = open("void.cfg", "w")
-        cf.write("lx = %f\n" % (x.max() + 5))
-        cf.write("ly = %f\n" % (y.max() + 5))
-        cf.write("lz = %f\n" % (z.max() + 5))
-        cf.write("num_pixels = %i\n" % len(x))
-        cf.write("map_nx = %i\n" % npx_x)
-        cf.write("map_ny = %i\n" % npx_y)
-        cf.write("map_nz = %i\n" % npx_z)
+        cf.write("lx = %f\n" % self.lx)
+        cf.write("ly = %f\n" % self.ly)
+        cf.write("lz = %f\n" % self.lz)
+        cf.write("num_pixels = %i\n" % len(xx))
+        cf.write("map_nx = %i\n" % self.npx_x)
+        cf.write("map_ny = %i\n" % self.npx_y)
+        cf.write("map_nz = %i\n" % self.npx_z)
         cf.write("corr_var_s = 0.23\n")
         cf.write("corr_l_perp = 5\n")
         cf.write("corr_l_para = 5\n")
@@ -194,47 +191,43 @@ class Interpolate:
 
         message1 = subprocess.run(['./dachshund.exe', 'void.cfg'], stdout=sys.stdout)
         print(message1)
-        newname = 'map{}.bin'.format(i)
+        newname = 'map{}.bin'.format(counter)
         message2 = subprocess.run(['mv', 'map.bin', newname], stdout=sys.stdout)
         print(message2)
 
-    def visualize_interpolation(self, i, x, y, z):
-        print(z)
-        npx_x = int(np.floor((x.max() - x.min()) / 2.))
-        npx_y = int(np.floor((y.max() - y.min()) / 2.))
-        npx_z = int(np.floor((z.max() - z.min()) / 2.))
-        gs = (npx_x, npx_y, npx_z)
+        self.visualize_interpolation(2)
 
-        xx = np.floor((x - x.min()) / 2)
-        yy = np.floor((y - y.min()) / 2)
-        zz = np.abs((z - z.min()) / 2 - z[i]) ** 2 / 0
-        print(zz)
-        plt.scatter(xx, yy, s=zz, c='black')
+    def visualize_interpolation(self, i, counter=0):
+        gs = (self.npx_x, self.npx_y, self.npx_z)
 
-        m = np.fromfile('map{}.bin'.format(i))
+        xline = np.linspace(0, self.lx, self.npx_x)
+        yline = np.linspace(0, self.ly, self.npx_y)
+        zline = np.linspace(0, self.lz, self.npx_z)
+
+        ixs = np.where(np.abs(self.chunk[:, 3] - zline[i]) <= 2)[0]
+
+        # plot the ith slice along z
+        m = np.fromfile('map{}.bin'.format(counter))
         m_grid = m.reshape(gs)
-        plt.imshow(m_grid[:, :, 1])
+        plt.pcolormesh(xline, yline, m_grid[:, :, i].T)
         cbar = plt.colorbar()
         cbar.set_label('delta', rotation=270)
+
+
+        # overlay the data points
+        plt.scatter(self.chunk[:, 0], self.chunk[:, 1], cmap=plt.cm.viridis)
+
         plt.xlabel('x')
         plt.ylabel('y')
         plt.show()
 
-    def visualize3d(self, x, y, z):
-        trace = go.Scatter3d(
-            x=x,
-            y=y,
-            z=z,
-            mode='markers',
-            marker=dict(
-                size=2,
-                opacity=0.5
-            )
-        )
-        fig = go.Figure(data=[trace])
-        plot(fig)
 
 
 if __name__ == '__main__':
-    interpolate = Interpolate('delta_transmission_RMplate.fits', sample_rate=1, noise=0.1)
-    interpolate.process_one(viz=True)
+    interpolate = Interpolate('../../delta_transmission_RMplate.fits')
+    interpolate.get_locations()
+    interpolate.get_matrix()
+
+    interpolate.get_data(skewers_perc=1., noise=0.1)
+    # interpolate.visualize3d(plot_1d=True, plot_2d=True)
+    interpolate.process_box(xcuts=[-30, 30], ycuts=[-30, 30], zcuts=[3600, 3630])
